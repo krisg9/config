a70c8b3a7d786f86d781144025d961d733f47f27
kristiyan <kristiyanbg7@gmail.com>
2023-03-29 16:43:58 +0200

    python lsp setup

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "lua_ls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "lua_ls", "jdtls", "jedi_language_server" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
