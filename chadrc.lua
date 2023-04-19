84bbcfbf2af12c976fa3a6b51c51f2ce131820f3
kristiyan <kristiyanbg7@gmail.com>
2023-04-19 18:44:40 +0200

    adjusted statusline

--- chadrc.lua
+++ chadrc.lua
@@ -7,6 +7,10 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "onedark",
   theme_toggle = { "onedark", "onedark" },
+  statusline = {
+    theme = "default",
+    separator_style = "arrow",
+  },
 
   -- hl_override = highlights.override,
   hl_add = highlights.add,
