6512271535a9907159dbc82b1f3f1c849a5156f1
kristiyan <kristiyanbg7@gmail.com>
2023-04-21 09:12:53 +0200

    prettier instead of prettierdw

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,9 +1,4 @@
-local present, null_ls = pcall(require, "null-ls")
-
-if not present then
-  return
-end
-
+local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
@@ -12,7 +7,7 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "scss" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "scss" } }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
